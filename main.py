1e7ab4efa1ec98c4c44274ec7c9d50b976235824
AbdullahHamadax <abdallahamada114@gmail.com>
2024-12-05 21:30:43 +0200

    Change env to environ

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 
 def main(context):
-    openai.api_key = os.env['OPENAI_API_KEY']
+    openai.api_key = os.environ['OPENAI_API_KEY']
 
     if not context.req.body:
         return context.res.json({"error": "No image provided"}, status_code=400)
