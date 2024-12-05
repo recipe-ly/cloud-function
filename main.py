56275618eafe0453452a201eeae4d935165dccc3
AbdullahHamadax <abdallahamada114@gmail.com>
2024-12-05 21:57:21 +0200

    Add context.log() to actually view the logs lol

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def main(context):
         )
 
         suggestions = response.choices[0]
+        context.log(suggestions)
 
         return context.res.json({"recipes": suggestions})
     except Exception as e:
