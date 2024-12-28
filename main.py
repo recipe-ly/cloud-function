edeb3eab856b643135c282683d65b874803fa867
waryoyo <youssef_hus@hotmail.com>
2024-12-29 00:35:02 +0200

    enhanced prompt

--- main.py
+++ main.py
@@ -28,7 +28,10 @@ def main(context):
             messages=[
                 {
                     "role": "system",
-                    "content": os.environ["INGREDIENTS_PROMPT"],
+                    "content": """You are an ingredient analyzer. Check the follow picture and return the list of ingredients based on everything you can see on the picture, i want you to return all the products, fruits and vegetables.
+i want you want to return everything in the structure of a json list of ingredients. Each Ingredient should have a name, type, amount and unit. the name will be the label it has on it if any,
+the type should be a simple name of what that ingredient is like is it pea or is some type of meat, in the case of spices please return a coma separated list of all the spices that the one in the picture might match.
+for the amount, please return a number, and unit return one from the standard units. please make sure to accurately get the type of the item to the precise thing it is.""",
                 },
                 {
                     "role": "user",
