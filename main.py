e90733679aa232cd438abae502b5b7552253e7b7
waryoyo <youssef_hus@hotmail.com>
2024-12-29 00:49:20 +0200

    prompt changes

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ from openai import OpenAI
 import base64
 import os
 from pydantic import BaseModel
+from enum import Enum
+
+class Unit(Enum):
+    GRAM = "g"
+    KILOGRAM = "kg"
+    LITER = "l"
+    MILLILITER = "ml"
+    CUP = "cup"
+    TABLESPOON = "tbsp"
+    TEASPOON = "tsp"
+    OUNCE = "oz"
+    POUND = "lb"
+    PIECE = "pcs"
+
 
 class Ingredient(BaseModel):
     name: str
+    product: str
     type: str
-    amount: str
-    unit: str
+    amount: int
+    unit: Unit
 
 class IngredientsList(BaseModel):
     ingredients: list[Ingredient]
@@ -29,8 +44,8 @@ def main(context):
                 {
                     "role": "system",
                     "content": """You are an ingredient analyzer. Check the follow picture and return the list of ingredients based on everything you can see on the picture, i want you to return all the products, fruits and vegetables.
-i want you want to return everything in the structure of a json list of ingredients. Each Ingredient should have a name, type, amount and unit. the name will be the label it has on it if any,
-the type should be a simple name of what that ingredient is like is it pea or is some type of meat, in the case of spices please return a coma separated list of all the spices that the one in the picture might match.
+i want you want to return everything in the structure of a json list of ingredients. Each Ingredient should have a name, product, type, amount and unit. the name will be the label it has on it if any,
+the product should be a simple name of what that ingredient is like is it pea or precisely what that item is this needs to precise, in the case of spices please return a coma separated list of all the spices that the one in the picture might match.
 for the amount, please return a number, and unit return one from the standard units. please make sure to accurately get the type of the item to the precise thing it is.""",
                 },
                 {
